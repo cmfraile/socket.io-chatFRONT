3402d2fbeaa0e50d775daaf11a6f6f771a8eaffb
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-01-19 11:30:03 +0100

    checkpoint

---
+++ src/app/componentes/ping/ping.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ping',
+  templateUrl: './ping.component.html',
+  styleUrls: ['./ping.component.sass']
+})
+export class PingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

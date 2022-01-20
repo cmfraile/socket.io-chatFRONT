797ff05ef956d5b54eaeea9b3d73a7a0b19d9a13
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-01-20 12:28:04 +0100

    nos vamos al back para tocar los pertinentes endpoints

--- src/app/componentes/registerlogin/registerlogin.component.ts
+++ src/app/componentes/registerlogin/registerlogin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder , FormGroup } from '@angular/forms';
 import { MatRadioChange } from '@angular/material/radio';
+import { WebsrestService } from 'src/app/servicios/websrest.service';
 
 @Component({
   selector: 'app-registerlogin',
@@ -12,12 +13,24 @@ export class RegisterloginComponent implements OnInit {
   forma!:FormGroup;
   accionestado:string|undefined = undefined;
   
-  constructor( private _fb:FormBuilder ){
+  constructor( private _fb:FormBuilder , private _wr:WebsrestService ){
     this.forma = this._fb.group({accion:[''],email:[''],nick:[''],pass1:[''],pass2:['']});
   }
 
   ngOnInit(): void {}
 
   changecb(evento:MatRadioChange){this.accionestado = evento.value ; this.forma.reset()}
+
+  botonform(){
+    switch(this.accionestado){
+      case undefined : return ;
+      case 'registro' :
+        console.log('registro botonform');
+        break;
+      case 'login' :
+        console.log('login botonform');
+        break;
+    }
+  }
 
 }

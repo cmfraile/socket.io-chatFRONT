b52fc78019be92a00dbcb3c262fb58ce47670096
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-01-26 13:05:51 +0100

    añadimos el modulo de registro

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _fb:FormBuilder ){}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { WebsrestService } from 'src/app/servicios/websrest.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  forma:FormGroup
+  errorform:boolean = false;
+  
+  constructor( private _fb:FormBuilder , private _wb:WebsrestService ){
+    this.forma = this._fb.group({correo:[''],nick:[''],pass1:[''],pass2:['']});
   }
+
+  registrarse(){
+    console.log(this.forma.value);
+    const { correo , nick , pass1 , pass2 } = this.forma.value;
+    if (correo == "" && nick == "" && pass1 == "" && pass2 == ""){return};
+    if(pass1 !== pass2){return};
+    this._wb.registro({correo,nick,pass:pass1}).subscribe({
+      next : () => {this.forma.reset() ; this.errorform = false},
+      error : () => {this.forma.reset() ; this.errorform = true}
+    })
+  }
+
+
+
+  ngOnInit(): void {}
 
 }

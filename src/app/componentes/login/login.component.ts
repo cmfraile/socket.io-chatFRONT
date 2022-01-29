2f17dc6245af58ccf7c50e77ac4c70cf8cdd6d74
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-01-29 17:12:16 +0100

    extraordinaria tarde de codigo explorando las posibilidades de los guards. Muy didactico, la verdad

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { WebsrestService } from 'src/app/servicios/websrest.service';
 
 @Component({
@@ -11,14 +12,20 @@ export class LoginComponent implements OnInit {
 
   forma:FormGroup;
   
-  constructor( private _fb:FormBuilder , private _wr:WebsrestService ){
+  constructor( private _fb:FormBuilder , private _wr:WebsrestService , private _r:Router ){
     this.forma = this._fb.group({ correo:[''] , pass:[''] });
   }
 
   loginar(){
     const { correo , pass } = this.forma.value;
     this._wr.login({correo,pass}).subscribe({
-      next: console.log , error: console.log , complete : () => {this.forma.reset()}
+      next: (resp:any) => {
+        console.log(resp);
+        localStorage.clear();
+        localStorage.setItem('token',resp.token);
+        localStorage.setItem('expiracion',new Date().setSeconds(resp.expiracion).toString());
+        this._r.navigate(['user'])
+      } , error: console.log , complete : () => {this.forma.reset()}
     })
   }
 
--- src/app/guardas/loginguard.guard.ts
+++ src/app/guardas/loginguard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,11 +7,34 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class LoginguardGuard implements CanActivate {
+  
+  constructor(private _r:Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      
+      /*return new Promise( (rs,rj) => {
+        const tokenobj = {token:localStorage['token'],expiracion:parseInt(localStorage['expiracion'])};
+        const movida:any = {
+          presente:new Date(),
+          futuro:new Date(tokenobj.expiracion)
+        };
+        if(Date.now() > movida.futuro){console.log(true)}else{console.log(false)};
+        if(!!localStorage['cipote']){return rj()};
+        
+        /*if(Date.now() > new Date(tokenobj.expiracion).getMilliseconds() ){
+          localStorage.clear() ; this._r.navigate(['']); rj(false);
+        }* /
+
+        rs(true);
+      });*/
+
       return new Promise( (rs,rj) => {
-        console.log(localStorage);
+        const fallo = () => { localStorage.clear() ; this._r.navigate(['']) ; rs(false) }
+        const expiracion = parseInt(localStorage['expiracion']);
+        if(!expiracion){fallo()};
+        if(Date.now() > expiracion){fallo()};
         rs(true);
       });
   }

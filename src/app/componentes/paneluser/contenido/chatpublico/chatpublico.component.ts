f6adc2a58fea4d1a1d7a7c624ba38101a7ab3433
Carlos Fraile <carlosfrailegomez93@gmail.com>
2022-02-21 11:30:03 +0100

    achuche

--- src/app/componentes/paneluser/contenido/chatpublico/chatpublico.component.ts
+++ src/app/componentes/paneluser/contenido/chatpublico/chatpublico.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsrestService } from 'src/app/servicios/websrest.service';
 
 @Component({
   selector: 'app-chatpublico',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatpublicoComponent implements OnInit {
 
-  constructor() { }
+  arraymsg:any[] = [];
+
+  
+  constructor( private _wr:WebsrestService ){
+    this._wr.socket.on('msgpublicpoke',(cb:any[]) => {
+      const myid:string = localStorage.getItem('id_user') || "";
+      cb.map( (x:any) => {
+        if(x.id == myid){x['propiedad'] = true}else{x['propiedad'] = false}
+      });
+    });
+  }
 
   ngOnInit(): void {}
 
@@ -25,8 +36,7 @@ export class ChatpublicoComponent implements OnInit {
 
   envio(msg:string){
     msg = msg.trim();
-    
-    console.log(msg,localStorage.getItem('id_user'))
+    console.log(msg)
   };
 
 }
